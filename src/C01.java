b41a173cce194c1f030716442bd789e430f294f7
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2022-09-25 12:25:28 +0200

    C01 Variables Scan

--- src/C01.java
+++ src/C01.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class C01 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello GitHub");
+        // Get a double and int numbers from user and print their sum and product
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter an integer number : ");
+        int integerNumber= scan.nextInt();
+
+        System.out.println("Please enter a decimal number : ");
+        double decimalNumber= scan.nextDouble();
+
+        System.out.println("Product of the entered numbers:" + integerNumber*decimalNumber);
+        System.out.println("Sum of the entered numbers:" + (integerNumber+decimalNumber));
+
+        scan.close();
     }
 }
