d8c2240012da2c3806ab19f929e7f91b6be17bc6
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2022-09-25 13:35:07 +0200

    C03 Print char variable

--- src/C03.java
+++ src/C03.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class C03 {
 
     public static void main(String[] args) {
 
-        System.out.println("C03");
+        // Get a letter from the user and print the next three letters in the alphabet
 
-        System.out.println("C03 satir eklendi");
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Please enter a letter");
+        char enteredLetter=scan.next().charAt(0);
+
+        System.out.println("Entered letter is : " + enteredLetter);
+
+        System.out.println("The next letter in the alphabet is : " + (char)(enteredLetter+1));
+        System.out.println("The second next letter in the alphabet is : " + (char)(enteredLetter+2));
+        System.out.println("The third next letter in the alphabet is : " + (char)(enteredLetter+3));
+
+        scan.close();
     }
 
 }
