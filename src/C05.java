e145f1a195f317c110418ca1bc3a2325ba5ac814
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2022-09-26 18:09:53 +0200

    C05 cating int

--- src/C05.java
+++ src/C05.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class C05 {
     public static void main(String[] args) {
 
-        System.out.println("C05");
+        // Get two double numbers from the user and divide the first number by the second number,
+        // then print the integer part of the result (quotient) of the division operation
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("enter two double numbers, \npush ENTER between them");
+        double number1= scan.nextDouble();
+        double number2= scan.nextDouble();
+
+        int result= (int)(number1/number2);
+
+        System.out.println("Integer part of the dıvısıon operatıon: " + result);
+
+        scan.close();
     }
 }
