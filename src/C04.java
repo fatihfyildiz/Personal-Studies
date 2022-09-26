c8fbc1138b0c078ed8f7282b0d30aaadf4a6287f
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2022-09-26 18:00:07 +0200

    C04 Casting Byte

--- src/C04.java
+++ src/C04.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class C04 {
 
     public static void main(String[] args) {
 
-        System.out.println("C04");
+        // Get a number from the user and no matter what the user enters,
+        // convert that number to a number between -128 and 127 and then print it.
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        double girilenSayi= scan.nextDouble();
+
+        byte convertedNumber= (byte)girilenSayi; // in order to convert between -128 and 127, casting to byte is done
+
+        System.out.println("The equivalent of the entered number in the system is : " + convertedNumber);
+
+        scan.close();
     }
 }
