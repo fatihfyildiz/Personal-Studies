33dd993bab72532930ae7ff308d079c3a229b855
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2022-10-21 23:12:03 +0200

    C23 Ternary ==> condition ? true : false

--- src/C23.java
+++ src/C23.java
@@ -10,6 +10,7 @@ public class C23 {
          */
 
         Scanner scan= new Scanner(System.in);
+
         System.out.println("Please enter an integer number");
         int number= scan.nextInt();
 
